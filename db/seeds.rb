246a93a781e78faf0c5b2a42729713956d94267f
Michael <michael@Michaels-MacBook-Pro.local>
2020-12-23 16:11:29 -0500

    add seed price data for the five product ids provided

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+Price.create([
+  {
+    product_id: 13860428,
+    currency_code: "USD",
+    value: 9.99
+  },
+  {
+    product_id: 54456119,
+    currency_code: "USD",
+    value: 1.99
+  },
+  {
+    product_id: 13264003,
+    currency_code: "USD",
+    value: 2.99
+  },
+  {
+    product_id: 12954218,
+    currency_code: "USD",
+    value: 4.99
+  }
+])
